047a70fd7e7fc17f53fc94ec5185753dcc61097c
CustomWasTaken <60800739+CustomWasTaken@users.noreply.github.com>
2020-02-22 21:37:20 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NjgwOTQyMTMyOTk0OTAwMDU5.XlHgcw.UgPJD1iJJ6ALkYPxnNmtdr7xkAs';
+
+const PREFIX = '/';
+
+var version = '1.0.9';
+
+
+bot.on('ready', () => {
+    console.log('Ryyan just got updated.');
+    bot.user.setActivity('WATCHING HUGOS COMMUNITY, {
+        type: 'TEST'
+    }).catch(console.error);
+
+
+})
+
+bot.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find(channel => channel.name === "welcome");
+    if (!channel) return;
+
+    channel.send(`Welcome to our server, ${member}, please read the rules in the rules channel!`)
+});
+
+
+
+
+
+bot.on('message', message => {
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0]) {
+        case 'ping':
+            message.reply('pong!');
+            break;
+        case 'hello':
+            message.reply('Hello there!');
+            break;
+        }
+});
+
+        bot.login(token);
